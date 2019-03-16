42e5f98cd5b1170cafbf24a389447de3e9687a03
Viay Jonnavithula <mrjvijay@gmail.com>
2019-03-15 22:19:00 -0600

    Initial commit of the project MVCDemo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCAngular.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
